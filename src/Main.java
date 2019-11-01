9b0792a510f46ed3077a3c038e72e0a47dd82f6f
lihuigang <35155212+lihuigang@users.noreply.github.com>
2019-11-01 11:43:49 +0800

    first

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Map;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashMapTest<String,String> mapTmp= new HashMapTest<String,String>();
+        mapTmp.put("2","2");
+
+        System.out.println("length22222222:"+mapTmp.getTable().length);
+        mapTmp.put("3","2");
+        mapTmp.put("4","2");
+        mapTmp.put("5","2");
+        mapTmp.put("6","2");
+        mapTmp.put("7","2");
+        mapTmp.put("8","2");
+        mapTmp.put("9","2");
+        mapTmp.put("10","2");
+        mapTmp.put("11","2");
+        mapTmp.put("12","2");
+        mapTmp.put("13","2");
+        mapTmp.put("14","2");
+        mapTmp.put("15","2");
+        mapTmp.put("16","2");
+        mapTmp.put("17","2");
+        mapTmp.put("17222222323424321dfsasdfsadfsadf","2");
+        mapTmp.put("17222222323424321dfsasdfsadfsadf1111111111111111111111111111111111111111111111","2");
+        mapTmp.put("1","2");
+
+        int tab_len=mapTmp.getTable().length;
+        int hash = mapTmp.getHash("17222222323424321dfsasdfsadfsadf");
+        int i = (tab_len-1)&hash;
+        System.out.println("length:"+tab_len);
+        System.out.println("hash:"+hash);
+        System.out.println("index:"+i);
+        System.out.println("123455:"+mapTmp.getTable()[i]);
+
+        System.out.println(mapTmp.get("3"));
+    }
+}
---
+++ src/MaxArrayTest.java
@@ -0,0 +1,86 @@
+public class MaxArrayTest {
+
+    class InfoA{
+        private int maxLeft;
+        private int maxRight;
+        private int sum;
+
+        public InfoA(int maxLeft,int maxRight,int sum){
+            this.maxLeft=maxLeft;
+            this.maxRight=maxRight;
+            this.sum=sum;
+        }
+
+        public int getMaxLeft(){
+            return this.maxLeft;
+        }
+
+        public int getMaxRight(){
+            return this.maxRight;
+        }
+
+
+        public int getSum(){
+            return this.sum;
+        }
+    }
+
+
+
+
+    public InfoA getInfo(int[] a,int low,int mid,int high){
+
+
+        int maxLeft=-1;
+        int maxRight=-1;
+        int left_sum=-10000;
+        int right_sum=-10000;
+        int sum=0;
+        for (int i=mid;i>=low;i--){
+            sum+=a[i];
+            if (sum>left_sum){
+                left_sum=sum;
+                maxLeft=i;
+            }
+        }
+
+        sum=0;
+        for (int i=mid+1;i<=high;i++){
+            sum+=a[i];
+            if (sum>right_sum){
+                right_sum=sum;
+                maxRight=i;
+            }
+        }
+        return new InfoA(maxLeft,maxRight,left_sum+right_sum) ;
+    }
+
+
+    public InfoA getMax(int[] a,int low,int high){
+
+        if (low == high){
+            return new InfoA(low,high,a[low]);
+        }else{
+            int mid=(low+high)/2;
+            InfoA leftInfo=getMax(a,low,mid);
+            InfoA rightInfo=getMax(a,mid+1,high);
+            InfoA crossInfo=getInfo(a,low,mid,high);
+            if(leftInfo.getSum()>=rightInfo.getSum()&&leftInfo.getSum()>=crossInfo.getSum()){
+                return leftInfo;
+            }else if(rightInfo.getSum()>=leftInfo.getSum()&&rightInfo.getSum()>=crossInfo.getSum()){
+                return rightInfo;
+            }else{
+                return crossInfo;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+        MaxArrayTest aa= new MaxArrayTest();
+        int[] a = {1,2,-1,4,-9,1,3,9};
+        InfoA infoA=aa.getMax(a,0,7);
+        System.out.println(infoA.getSum());
+
+    }
+}
